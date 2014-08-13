bece55877198c95631230f6349e49766ed272d9a
Milton Madanda <madandat@gmail.com>
2014-08-13 11:01:41 +0200

    show categories on home page

--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,13 @@
+import os
+from gitmodel.workspace import Workspace
 from pyramid.view import view_config
+from cms import models as cms_models
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    return {'project': 'cms'}
+    repo_path = os.path.join(request.registry.settings['git.path'], '.git')
+    ws = Workspace(repo_path)
+    models = ws.import_models(cms_models)
+    categories = models.Category().all()
+    return {'categories': categories}
