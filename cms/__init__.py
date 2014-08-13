7a913ce7400caf315f9ef68b898c7b0ea3a0bd4f
Milton Madanda <madandat@gmail.com>
2014-08-13 11:00:10 +0200

    add models

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,4 +1,9 @@
+import os
+import pygit2
+
 from pyramid.config import Configurator
+from gitmodel.workspace import Workspace
+from cms.models import Page, Category
 
 
 def main(global_config, **settings):
@@ -9,4 +14,12 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
+
+    repo_path = settings['git.path'].strip()
+    pygit2.init_repository(repo_path, False)
+
+    ws = Workspace(os.path.join(repo_path, '.git'))
+    ws.register_model(Page)
+    ws.register_model(Category)
+
     return config.make_wsgi_app()
