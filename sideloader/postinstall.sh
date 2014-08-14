b61a256a3369e4edbaf1606b478fdafd4dd09cf5
Milton Madanda <madandat@gmail.com>
2014-08-14 13:43:13 +0200

    ensure the app is installed too

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -1,4 +1,4 @@
 pip="${VENV}/bin/pip"
 cd /var/praekelt/unicore-cms/ && echo `pwd` && ./install_libgit2 && cd ~/
 
-$pip install cffi praekelt-python-gitmodel
+$pip install cffi praekelt-python-gitmodel /var/praekelt/unicore-cms/
