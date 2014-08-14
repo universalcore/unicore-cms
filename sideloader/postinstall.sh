38c24e5bc6b335e3165a92a43ca57fab5aea1501
Milton Madanda <madandat@gmail.com>
2014-08-14 13:09:19 +0200

    change to user home dir

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -1,3 +1,3 @@
-cd /var/praekelt/unicore-cms/ && echo `pwd` && ./install_libgit2 && cd /
+cd /var/praekelt/unicore-cms/ && echo `pwd` && ./install_libgit2 && cd ~/
 
 $PIP install cffi praekelt-python-gitmodel
