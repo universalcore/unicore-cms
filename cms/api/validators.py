880b0b2cf8c6584329e711b019be4beacf3893e7
Milton Madanda <madandat@gmail.com>
2014-08-27 13:58:14 +0200

    remove duplication in validator

--- cms/api/validators.py
+++ cms/api/validators.py
@@ -1,14 +1,9 @@
 import json
 
 
-def update_validated_field(request, data, key):
-    if key in data and data[key] is not None:
-        request.validated[key] = data[key]
-
-
 def validate_required_field(request, data, key):
     if key in data and data[key] is not None:
-        update_validated_field(request, data, key)
+        request.validated[key] = data[key]
     else:
         request.errors.add('body', key, '%s is a required field.' % key)
 
