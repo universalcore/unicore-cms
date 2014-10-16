d4d241b61791e4ad6180d3e337ba41e3ad96ad7c
Simon de Haan <simon@praekeltfoundation.org>
2014-10-16 12:44:19 +0200

    tools for migration repo in ./repo and writing model file from spec

---
+++ migrate-repo-to-elastic-git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# halt on any error
+set -e
+
+mkdir -p unicore/content
+
+touch unicore/__init__.py
+touch unicore/content/__init__.py
+
+python -m elasticgit.tools \
+    migrate-gitmodel-repo \
+    ./repo/ unicore.content.models
+
+python -m elasticgit.tools \
+    load-schema ./repo/unicore.content.models/GitPageModel.avro.json \
+                ./repo/unicore.content.models/GitCategoryModel.avro.json \
+                --map-field uuid=elasticgit.models.UUIDField \
+                --rename-model GitPageModel=Page \
+                --rename-model GitCategoryModel=Category \
+                > ./unicore/content/models.py
+
+mv repo/unicore.content.models/*.avro.json ./unicore/
+
+python -c 'from unicore.content import models; print models.Page; print models.Category;'
