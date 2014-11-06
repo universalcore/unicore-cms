f202d03ae548c8130b7532af986273d9ded5b018
Milton Madanda <madandat@gmail.com>
2014-11-06 15:08:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'requirements.txt')) as f:
     requires = filter(None, f.readlines())
 
 setup(name='unicore-cms',
-      version='1.0.9',
+      version='1.0.10',
       description='JSON based CMS for Universal Core',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
