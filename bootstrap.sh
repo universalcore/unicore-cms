709cee88f6b9ee9c56327d5f7efa6e30f741ace9
Milton Madanda <madandat@gmail.com>
2014-11-25 09:14:02 +0200

    added log messages to script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,11 @@ source ve/bin/activate
 git pull
 pip install -e .
 pip install --upgrade elastic-git
+echo 'Done installing requirements.'
+echo 'Cloning repo..'
 git clone https://github.com/universalcore/unicore-cms-content-ffl-tanzania repo
+echo 'Creating indexes..'
 eg-tools resync -c development.ini -m unicore.content.models.Category -f mappings/category.mapping.json -r true
 eg-tools resync -c development.ini -m unicore.content.models.Page -f mappings/page.mapping.json
+echo 'Starting webserver..'
 pserve development.ini --reload
