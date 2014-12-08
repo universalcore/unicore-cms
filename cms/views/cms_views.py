553dc44373955734ea5c2835eb0ba000254803c6
nathanbegbie <nathanbegbie@gmail.com>
2014-12-08 12:29:53 +0200

    created 404 template and changed not found config

--- cms/views/cms_views.py
+++ cms/views/cms_views.py
@@ -17,6 +17,8 @@ from cms.views.base import BaseCmsView
 from unicore.content.models import Category, Page, Localisation
 from utils import EGPaginator
 
+from pyramid.view import notfound_view_config
+
 CACHE_TIME = 'default_term'
 
 
@@ -234,3 +236,7 @@ class CmsViews(BaseCmsView):
             'query': query,
             'p': p,
         }
+
+    @notfound_view_config(renderer='cms:templates/404.pt')
+    def notfound(request):
+        return {}
