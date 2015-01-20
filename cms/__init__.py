697a7ac2374bde66a60986c1a79928bc3ace4bd7
Simon de Haan <simon@praekeltfoundation.org>
2015-01-20 17:26:49 +0200

    use pyramid_celery & unicore.google.tasks

--- cms/__init__.py
+++ cms/__init__.py
@@ -29,6 +29,7 @@ def main(global_config, **settings):
     set_cache_regions_from_settings(settings)
     config = Configurator(settings=settings)
     config.include('cms')
+    config.configure_celery(global_config['__file__'])
     return config.make_wsgi_app()
 
 
