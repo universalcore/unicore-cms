c54ecb300d18b31f3bd643998dac2360d6466943
Simon de Haan <simon@praekeltfoundation.org>
2015-01-22 22:25:45 +0200

    make sure pyramid tasks load celery with the correct config

--- cms/tasks.py
+++ cms/tasks.py
@@ -1,10 +1,10 @@
-from celery.task import task
+from pyramid_celery import celery_app as app
 
 from elasticgit import EG
 from unicore.content.models import Page, Category, Localisation
 
 
-@task(ignore_result=True, serializer='json')
+@app.task(ignore_result=True, serializer='json')
 def fastforward(repo_path, index_prefix):
     workspace = EG.workspace(repo_path, index_prefix=index_prefix)
     workspace.fast_forward()
